5bb3f783061f3787d58198970b04187e8221f65a
andressa <andressaleal@lett.digital>
2024-08-12 00:38:41 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+let username = '';
+let socket;
+
+document.getElementById('setUsernameButton').addEventListener('click', function() {
+    const nameInput = document.getElementById('nameInput');
+    username = nameInput.value.trim();
+    if (username) {
+        document.getElementById('usernameModal').style.display = 'none';
+        connectWebSocket();
+    }
+});
+
+document.getElementById('sendButton').addEventListener('click', function() {
+    const input = document.getElementById('msgInput');
+    const mensagem = input.value.trim();
+    if (mensagem && socket) {
+        socket.send(mensagem);
+        input.value = '';
+    }
+});
+
+function connectWebSocket() {
+    socket = new WebSocket('ws://localhost:15000');
+
+    socket.onopen = function() {
+        if (username) {
+            socket.send(username); 
+        }
+    };
+
+    socket.onmessage = function(event) {
+        console.log("Mensagem recebida:", event.data);
+        const areaMensagem = document.getElementById('areaMensagem');
+        const newTextBox = document.createElement('div');
+        newTextBox.className = 'textBox';
+        newTextBox.innerText = event.data;
+        areaMensagem.appendChild(newTextBox);
+        areaMensagem.scrollTop = areaMensagem.scrollHeight;
+    };
+
+    socket.onerror = function(error) {
+        console.error('WebSocket erro:', error);
+    };
+
+    socket.onclose = function(event) {
+        console.log('WebSocket fechou:', event.reason);
+    };
+}
+
+window.onload = function() {
+    document.getElementById('usernameModal').style.display = 'flex';
+}
