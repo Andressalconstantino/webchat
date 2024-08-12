60c4bd7b4cdd14e7fcb063fe493b7c2845cc2f7b
andressa <andressaleal@lett.digital>
2024-08-12 17:00:50 -0300

    undo

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.getElementById('sendButton').addEventListener('click', function() {
 });
 
 function connectWebSocket() {
-    socket = new WebSocket('https://sly-marbled-yuzu.glitch.me');
+    socket = new WebSocket('wss://sly-marbled-yuzu.glitch.me:15000');
 
     socket.onopen = function() {
         console.log("WebSocket connection established.");
